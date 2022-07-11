d10682bd98720abe0f8d4ac4ed7bf21c331051f8
Michael Maurer <3191110276@users.noreply.github.com>
2022-07-11 03:00:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,6 +52,22 @@ resource "intersight_ntp_policy" "ntp" {
   }
 }
 
+resource "intersight_workflow_workflow_info" "workflow_workflow_info1" {
+  name         = "workflow_workflow_info1"
+  pause_reason = null
+  action       = "Create"
+  success_workflow_cleanup_duration = 2160
+  wait_reason                       = null
+  workflow_meta_type                = "SystemDefined"
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.myorg.results.0.moid
+  }
+  workflow_definition {
+    object_type = "workflow.WorkflowDefinition"
+    moid        = "62cb72a7696f6e2d314fa137"
+  }
+}
 
 resource "intersight_workflow_workflow_info" "workflow_workflow_info1" {
   name         = "workflow_workflow_info1"
